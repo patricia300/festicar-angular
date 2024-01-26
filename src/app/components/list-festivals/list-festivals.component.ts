3e20d0fc0f237bce37fdb4b1fbcbb10957a887b1
rzfrsn <tafita.raza1@gmail.com>
2024-01-26 09:52:11 +0100

    add loading in festival listing

--- src/app/components/list-festivals/list-festivals.component.ts
+++ src/app/components/list-festivals/list-festivals.component.ts
@@ -11,18 +11,36 @@ import { FestivalsService } from 'src/app/services/festivals.service';
 export class ListFestivalsComponent implements OnInit {
   festivals: Festival[] = [];
   pageFestival?: Pageable<Festival>;
+  festivalsIsLoading: boolean = false;
 
   constructor(private festivalsService: FestivalsService){}
 
   ngOnInit(): void {
+    this.festivalsIsLoading = true;
+
     this.festivalsService.getAll().subscribe((pageFestival: Pageable<Festival>) => {
       this.pageFestival = pageFestival;
       this.festivals = pageFestival.content;
+      this.festivalsIsLoading = false;
+      window.scroll(0, 0);
     });
   }
 
   getTotalPages(): number {
     return this.pageFestival?.totalPages || 0;
+  }
+
+  onPaginationUpdated(currentPage: number) {
+    console.log('paginationUpdated', currentPage);
+
+    this.festivalsIsLoading = true;
+    this.festivalsService.getAll(currentPage).subscribe((pageFestival: Pageable<Festival>) => {
+      this.pageFestival = pageFestival;
+      this.festivals = pageFestival.content;
+      this.festivalsIsLoading = false;
+      window.scroll(0, 0);
+      console.log("festivals : ", this.pageFestival);
+    });
   }
 
 }
