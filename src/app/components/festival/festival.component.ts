c8890c97dca5dd96936f9e19652fddf7a6da1b49
rzfrsn <tafita.raza1@gmail.com>
2024-01-26 09:51:08 +0100

    add computed remaining seats

--- src/app/components/festival/festival.component.ts
+++ src/app/components/festival/festival.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Festival } from 'src/app/interfaces/festival';
+import { PointPassageCovoiturages } from 'src/app/interfaces/point-passage-covoiturages';
 
 @Component({
   selector: 'app-festival',
@@ -8,4 +9,15 @@ import { Festival } from 'src/app/interfaces/festival';
 })
 export class FestivalComponent {
   @Input({required: true}) festival!: Festival;
+
+  selectedPointPassage?: PointPassageCovoiturages = undefined;
+
+  getRemainingSeats(): number {
+    let total = 0;
+    this.festival.offreCovoiturages.forEach((offre) => {
+      total += offre.nombrePlaces;
+    })
+
+    return total;
+  }
 }
