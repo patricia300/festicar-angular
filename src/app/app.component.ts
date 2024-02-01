6bb09ccbc95f8badb097d95a6a1599cb86843503
rzfrsn <tafita.raza1@gmail.com>
2024-02-01 01:08:12 +0100

    update show commune search input handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FestivalsService } from './services/festivals.service';
 import { PanierService } from './services/panier.service';
 import { DomaineService } from './services/domaine.service';
@@ -11,63 +11,28 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  // festivals: Festival[] = [];
-  // festivalsByCommune: Festival[] = [];
-  // festivalsByDate: Festival[] = [];
-  // festivalsByFilter: Festival[] = [];
+export class AppComponent implements OnInit, OnChanges  {
+  showFestivalSearchInput: boolean = false;
+  showCommuneSearchInput: boolean = false;
 
-  domainePrincipals?: DomainePrincipal[];
-
-  constructor(
-    private routeService: RouteService,
-    private festivalsService: FestivalsService,
-    private panierService: PanierService,
-    private domainService: DomaineService,
-    private http: HttpClient) { }
+  constructor(private routeService: RouteService) { }
 
   ngOnInit(): void {
-    // this.festivalsService.getAllFestivalsByCommune().subscribe((pageFestival: Pageable<Festival>) => {
-    //   this.festivalsByCommune = pageFestival.content;
-    //   console.log("festivalsByCommune : ", this.festivalsByCommune);
-    // });
-
-    // this.festivalsService.getAllFestivalsByDate().subscribe((pageFestival: Pageable<Festival>) => {
-    //   this.festivalsByDate = pageFestival.content;
-    //   console.log("festivalsByDate : ", this.festivalsByDate);
-    // });
-
-    // this.festivalsService.getAllFestivalByFiltre().subscribe((pageFestival: Pageable<Festival>) => {
-    //   this.festivalsByFilter = pageFestival.content;
-    //   console.log("festivalsByFilter : ", this.festivalsByFilter);
-    // });
-
-
-
-    // this.panierService.getAll().subscribe((pagePanier: Panier[]) => {
-    //   this.paniers = pagePanier;
-    //   console.log("paniers : ", this.paniers);
-    // });
-
-    // this.panierService.getCurrentPanier().subscribe((pagePanier: Panier) => {
-    //   this.currentPanier = pagePanier;
-    //   console.log("currentPanier : ", this.paniers);
-    // });
-
-
-
-    // this.domainService.getAll().subscribe((pageDomaine: DomainePrincipal[]) => {
-    //   this.domainePrincipals = pageDomaine;
-    //   console.log("domainePrincipals : ", this.domainePrincipals);
-    // });
+    this.updateShowCommuneSearchInput();
+    this.updateShowFestivalSearchInput();
   }
 
-  showFestivalSearchInput() {
-    return this.routeService.currentRoute.includes('/festivals');
+  ngOnChanges(changes: SimpleChanges): void {
+    this.updateShowCommuneSearchInput();
+    this.updateShowFestivalSearchInput();
   }
 
-  showCommuneSearchInput() {
-    return this.routeService.currentRoute.includes('/covoiturages');
+  updateShowFestivalSearchInput() {
+    this.showFestivalSearchInput = this.routeService.currentRoute.includes('/festivals');
+  }
+
+  updateShowCommuneSearchInput() {
+    this.showCommuneSearchInput = this.routeService.currentRoute.includes('/covoiturages');
   }
 
 }
