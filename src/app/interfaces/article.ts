45716a2dba1f7b929991b1d4fd7e61e915fed11c
rzfrsn <tafita.raza1@gmail.com>
2024-02-01 01:12:05 +0100

    update article interface

--- src/app/interfaces/article.ts
+++ src/app/interfaces/article.ts
@@ -1,3 +1,5 @@
+import { Festival } from "./festival";
+import { OffreCovoiturage } from "./offre-covoiturage";
 import { Panier } from "./panier";
 import { PointPassageCovoiturages } from "./point-passage-covoiturages";
 
@@ -5,10 +7,20 @@ export interface Article {
   id: number,
   panier: Panier,
   pointPassageCovoiturage: PointPassageCovoiturages,
-  quantite: number
+  quantite: number,
+  festival: Festival
 }
 
 export interface AjoutArcticle {
   idPointPassage: number,
   quantite: number
+}
+
+export interface ArticleCard {
+  id: number,
+  festival: Festival,
+  quantite: number,
+  offreCovoiturage?: OffreCovoiturage,
+  pointPassageCovoiturage?: PointPassageCovoiturages,
+  checked?: boolean
 }
