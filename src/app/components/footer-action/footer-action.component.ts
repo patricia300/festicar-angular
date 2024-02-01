6a483ce409ec64a5ea30b73a4ee20c4bad4b96d6
rzfrsn <tafita.raza1@gmail.com>
2024-02-01 01:04:50 +0100

    add props buttonText of footer action

--- src/app/components/footer-action/footer-action.component.ts
+++ src/app/components/footer-action/footer-action.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class FooterActionComponent {
   @Input({required: true}) disabled: boolean = true;
+  @Input() buttonText: string = 'ajouter au panier';
   @Output() clickRightButton = new EventEmitter<null>();
 
   onClickButton() {
